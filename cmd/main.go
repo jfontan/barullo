018d392779cd60885ef2f295a1dc1b6b1eb03ab1
Javi Fontan <jfontan@gmail.com>
2019-05-27 10:21:12 +0200

    WIP
    Signed-off-by: Javi Fontan <jfontan@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 var (
 	channelNum      = 1
 	bitDepthInBytes = 2
-	bufferSize      = 64 * 10
+	bufferSize      = 64 * 1000
 )
 
 const (
@@ -41,16 +41,16 @@ func mPortaudio() {
 	}
 	defer stream.Stop()
 
-	f, err := os.Open("./assets/never_gonna_give_you_up.json")
+	f, err := os.Open("./assets/take_on_me.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	e1 := barullo.GetEventsFromMidi(5, sampleRate, f)
+	e1 := barullo.GetEventsFromMidi(0, sampleRate, f)
 	f.Seek(0, os.SEEK_SET)
-	e2 := barullo.GetEventsFromMidi(7, sampleRate, f)
+	e2 := barullo.GetEventsFromMidi(1, sampleRate, f)
 	f.Seek(0, os.SEEK_SET)
-	e3 := barullo.GetEventsFromMidi(10, sampleRate, f)
+	e3 := barullo.GetEventsFromMidi(4, sampleRate, f)
 
 	eLength1 := e1[len(e1)-1].Offset + 6000
 	eLength2 := e2[len(e2)-1].Offset + 6000
@@ -81,8 +81,10 @@ func mPortaudio() {
 	lp3 := barullo.NewLPFilter(env3, 8000.3, 1.1)
 
 	mixer := barullo.NewMixer([]barullo.Node{lp1, lp2, lp3}, []float64{0.8, 0.8, 0.8})
+	// mixer := barullo.NewMixer([]barullo.Node{lp1, lp2, lp3}, []float64{0.0, 0.0, 1.0})
 
 	var sampleOffset int64
+	sampleOffset = 44100 * 8
 	for {
 		mixer.Get(int(sampleOffset), buf)
 
