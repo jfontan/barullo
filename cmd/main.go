97c1d69224927f5ef78f4b528ebfc5ba1d3f565a
Antonio Navarro Perez <antnavper@gmail.com>
2019-05-28 09:35:30 +0200

    Add Take On Me and Noise generator
    Signed-off-by: Antonio Navarro Perez <antnavper@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 
@@ -20,7 +21,8 @@ const (
 )
 
 func main() {
-	mPortaudio()
+	//mPortaudio()
+	mPortaudioTakeOnMe()
 }
 
 func mPortaudio() {
@@ -83,6 +85,148 @@ func mPortaudio() {
 	mixer := barullo.NewMixer([]barullo.Node{lp1, lp2, lp3}, []float64{0.8, 0.8, 0.8})
 
 	var sampleOffset int64
+	for {
+		mixer.Get(int(sampleOffset), buf)
+
+		sampleOffset += int64(bufferSize)
+
+		f64ToF32Copy(out, buf)
+
+		if err := stream.Write(); err != nil {
+			log.Printf("error writing to stream : %v\n", err)
+		}
+	}
+}
+
+var channels = []struct {
+	events func(io.Reader) []barullo.Event
+	source func([]barullo.Event, int) *barullo.LPFilter
+	volume float64
+}{
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(0, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewPulse(0.25, sampleRate, seq)
+			env := barullo.NewEnvelope(2000/4, 2000/4, 0.8, 10000/4, sig, seq)
+
+			return barullo.NewLPFilter(env, 300.3, 1.1)
+		},
+		volume: 0.4,
+	},
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(2, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewPulse(0.2, sampleRate, seq)
+			env := barullo.NewEnvelope(2000/4, 2000/4, 0.8, 10000/4, sig, seq)
+			return barullo.NewLPFilter(env, 5000.3, 6.2)
+		},
+		volume: 0.05,
+	},
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(3, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewTriangle(0.8, sampleRate, seq)
+			env := barullo.NewEnvelope(100, 0, 1.0, 500, sig, seq)
+
+			return barullo.NewLPFilter(env, 8000.3, 6.2)
+
+		},
+		volume: 0.1,
+	},
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(4, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewSignal(barullo.Sin, sampleRate, seq)
+			env := barullo.NewEnvelope(1000/4, 1000/4, 0.8, 10000/4, sig, seq)
+			return barullo.NewLPFilter(env, 5000.3, 6.2)
+		},
+		volume: 0.1,
+	},
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(5, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewPulse(0.25, sampleRate, seq)
+			env := barullo.NewEnvelope(2000/4, 2000/4, 0.8, 10000/4, sig, seq)
+
+			return barullo.NewLPFilter(env, 600.3, 1.1)
+		},
+		volume: 0.2,
+	},
+	{
+		events: func(f io.Reader) []barullo.Event {
+			return barullo.GetEventsFromMidi(7, sampleRate, f)
+		},
+		source: func(evts []barullo.Event, length int) *barullo.LPFilter {
+			seq := barullo.NewSequence(length, evts)
+			sig := barullo.NewSignal(barullo.Noise, sampleRate, seq)
+			env := barullo.NewEnvelope(2000/4, 2000/4, 0.8, 10000/4, sig, seq)
+			return barullo.NewLPFilter(env, 5000.3, 6.2)
+		},
+		volume: 0.2,
+	},
+}
+
+func mPortaudioTakeOnMe() {
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	buf := make([]float64, bufferSize)
+	out := make([]float32, bufferSize)
+
+	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, bufferSize, &out)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stream.Close()
+
+	if err := stream.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer stream.Stop()
+
+	f, err := os.Open("./assets/take_on_me.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var allEvents [][]barullo.Event
+	var maxlen int
+	for _, ch := range channels {
+		events := ch.events(f)
+		allEvents = append(allEvents, events)
+		f.Seek(0, os.SEEK_SET)
+		eLength := events[len(events)-1].Offset + 6000
+		if eLength > maxlen {
+			maxlen = eLength
+		}
+	}
+
+	var nodes []barullo.Node
+	var volumes []float64
+	for i, ch := range channels {
+		nodes = append(nodes, ch.source(allEvents[i], maxlen))
+		volumes = append(volumes, ch.volume)
+	}
+
+	mixer := barullo.NewMixer(nodes, volumes)
+
+	var sampleOffset int64
+	sampleOffset = 44100 * 8
 	for {
 		mixer.Get(int(sampleOffset), buf)
 
